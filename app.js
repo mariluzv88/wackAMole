49e9fd06838000697f6bd81a0b0dea6734909f5c
Mariluz Vargas <mariluzv88@gmail.com>
2023-06-27 16:32:30 -0400

    js base logic and stretch goals done

---
+++ app.js
@@ -0,0 +1,57 @@
+// variable for current tile
+// variable for decoy tile
+// variable score
+// variable loss false
+// window onload function
+    // game init function
+        // set grid for loop < num of tiles
+        // call mole divs from dom
+        // event listener click is true select
+        // call game board from dom append child
+    
+    // set interval || settimeout  mole 1 millisec
+    // set interval || settimeout  decoy 2 millisec
+// 
+
+// randon tile function
+      
+
+    // math.random * num of tiles
+    // return as string for class id
+
+
+// mole location function
+    //  if condition gameover return
+
+    // clear previous moles if condition innerHMTL 
+    // create element img for mole
+    // src link
+
+    // variable random num function
+    // if conditional for mole/decoy occupying the same tile/num return
+    // tile location call dom idnum
+    // tile location append mole
+// 
+
+// decoy function
+    //  if condition gameover return
+
+    // if condition innerHTMl
+
+    // create image in dom
+    // src link
+
+    // variable random num function
+     // if conditional for mole/decoy occupying the same tile/num return
+    // tile location call dom idnum
+    // tile location append decoy
+// 
+
+// select function
+    //  if condition gameover return
+    // if conditional this assigned to cureent mole tile
+    // score +=
+    // call score from dom innertext score to string updating score
+    // else decoy update game over
+    // loss true
+
